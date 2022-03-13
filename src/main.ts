8e3fd7e75cdc6d212fccf70cc23619650b2d1ae6
Vectorx <hector_bayona@hotmail.com>
2022-03-12 19:58:35 -0500

    test

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,11 @@ async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
+    await exec.exec('ls')
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
     core.setOutput('time', new Date().toTimeString())
-
-    await exec.exec('ls')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
